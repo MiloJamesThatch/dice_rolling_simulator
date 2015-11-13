0a283d12dfac372902e3748be8061d7acf26c27e
David Isham <davidandrewisham@gmail.com>
2015-11-13 11:55:39 -0600

    Create dice0_1.py

---
+++ dicev0_1.py
@@ -0,0 +1,13 @@
+import random
+import sys
+import os
+
+# Dice Rolling Simulator v.1
+
+print("Welcome to the Dice Rolling Simulator v.1")
+print("")
+
+dice = random.sample([1,2,3,4,5,6], 1)
+print(dice)
+
+# need to add user input to ask whether to roll the dice or not
